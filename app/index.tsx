030008f30e3c5a26f9d99bd8250e657a20a9278f
redcliff-op <devyashjangid@gmail.com>
2024-09-10 00:14:01 +0530

    NativeWind: Init PostCSS for Web

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,11 @@ import { View, Text, Pressable } from 'react-native'
 import React, { memo } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
+import "../constants/styles.css"
 
 const index = memo(() => {
   return (
-    <SafeAreaView className='px-2 flex-1 bg-white'>
+    <SafeAreaView className='px-2 flex-1 justify-center items-center bg-blue-500'>
       <Pressable
         onPress={()=>{
           router.navigate(`/(tabs)`)
